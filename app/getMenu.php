2bd26f7a4955a94cbc7c72595eb436c34b665330
agentflare <samuel.wu.u1@gmail.com>
2014-10-04 16:15:50 -0500

    Update backend

---
+++ app/getMenu.php
@@ -0,0 +1,14 @@
+<?php
+	$link=mysqli_connnect("host","user","pass","db");
+	$menu=mysqli_query($link,"SELECT I.ItemName,I.ItemDescription,I.ImageURL, C.CategoryName FROM items AS I INNER JOIN categories AS C ON I.CategoryID=C.CategoryID ORDER BY CategoryID");
+	
+	$i=0;
+	while($row = mysqli_fetch_array($menu)) {
+		$ItemName[$i]=$row['ItemName'];
+		$ItemDesc[$i]=$row['ItemDescription'];
+		$ImageURL[$i]=$row['ImageURL'];
+		$CategoryName[$i]=$row['CategoryName'];
+		$i=$i+1;
+	}
+	
+?>
