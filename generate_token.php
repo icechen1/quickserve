8e9dc960df7f4d2e1263a47c0fddff17125fe7cc
agentflare <samuel.wu.u1@gmail.com>
2014-10-04 23:41:55 -0500

    debug

--- generate_token.php
+++ generate_token.php
@@ -1,5 +1,5 @@
 <?php
-	$link=parse_url(getenv("CLEARDB_DATABASE_URL"));
+	/*$link=parse_url(getenv("CLEARDB_DATABASE_URL"));
 	header('Acess-Control-Allow-Origin: *');
     $server = $url["host"];
     $username = $url["user"];
@@ -13,14 +13,14 @@
 	$userID=$_POST['userID'];
 	
 	
-	$date=time();
+	$date=time();*/
 	
 	$t=mysqli_query($link,"SELECT Max(OrderID)+1 FROM orders");
-	foreach($data)
+	/*foreach($data)
 	{
 		mysqli_query($link,"INSERT INTO orders('OrderDate','ItemID','UserID','isComplete') VALUES (".$date.",".$data['key'].",".$userID.","."0".")");
 		mysqli_query($link,"UPDATE orders SET Barcode= ".$t." FROM orders)");
-	}
+	}*/
 	echo $t;
 	
 ?>
