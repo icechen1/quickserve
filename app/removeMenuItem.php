f2e79403a0ed60d8b9907189820d2dcb745f636c
agentflare <samuel.wu.u1@gmail.com>
2014-10-04 21:50:23 -0500

    php!

--- app/removeMenuItem.php
+++
@@ -1,9 +0,0 @@
-<?php
-	$link=mysqli_connnect("host","user","pass","db");
-	
-	$itemID=$_POST['itemID'];
-	
-	mysqli_query($link,"DELETE FROM items WHERE itemID=".$itemID);
-	mysqli_query($link,"DELETE FROM restaurants_items WHERE itemID=".$itemID);
-	
-?>
