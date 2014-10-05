6d5b33a564eee6cd0cfbea904077ac47fb5b525d
Yu Chen Hou <houyuchen66@gmail.com>
2014-10-05 05:13:40 -0400

    node

--- index.js
+++ index.js
@@ -5,7 +5,12 @@ var io = require('socket.io')(http);
 var currentMenus = [];
 var id = 0;
 
-app.set('port', (process.env.PORT || 5000))
+app.set('port', (process.env.PORT || 5000));
+app.all('*', function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "X-Requested-With");
+  next();
+ });
 app.get('/', function(req, res){
   res.sendfile('index.html');
 });
